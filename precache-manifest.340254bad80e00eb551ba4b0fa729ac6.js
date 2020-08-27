d3171bb629f5bfd942c57191670706a487a5130d
Lakhwinder Singh <lakhwinder0199@gmail.com>
2020-08-27 21:53:25 +0530

    Updates

---
+++ precache-manifest.340254bad80e00eb551ba4b0fa729ac6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25878cba1a01b02cf4c2c8a3dbe32184",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "9c30a53d374a590bab7a",
+    "url": "/Portfolio/static/css/main.1d9871c5.chunk.css"
+  },
+  {
+    "revision": "1185ee2726788c695dd4",
+    "url": "/Portfolio/static/js/2.75abc4d6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.75abc4d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c30a53d374a590bab7a",
+    "url": "/Portfolio/static/js/main.79811d0c.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "8f5ce27564945d2c9a10ef827549a78c",
+    "url": "/Portfolio/static/media/1.8f5ce275.png"
+  },
+  {
+    "revision": "5c570427ee23f69853d28aec805eee79",
+    "url": "/Portfolio/static/media/2.5c570427.png"
+  },
+  {
+    "revision": "0251d8ee95aa6d1f3400faa3b46b4bcf",
+    "url": "/Portfolio/static/media/4.0251d8ee.png"
+  },
+  {
+    "revision": "774352deeaa60ebe04164a1e28489771",
+    "url": "/Portfolio/static/media/5.774352de.png"
+  },
+  {
+    "revision": "56d6c79b0181bf8fc40797d2e31fc952",
+    "url": "/Portfolio/static/media/profile.56d6c79b.jpg"
+  }
+]);
